a0d6aed9c6a3dcaaf0b3a07b9df785ac98769adc
Victoria <victorialors@hotmail.com>
2023-11-29 14:50:01 -0600

    Add photo to about tab

--- src/about.js
+++ src/about.js
@@ -3,6 +3,10 @@ const loadAbout = function() {
     let aboutContainer = document.createElement('div')
     let aboutUs = document.createElement('h1')
     let aboutContent = document.createElement('p')
+    let img = document.createElement('img')
+
+    // add attributes
+    img.src = "https://images.unsplash.com/photo-1571903244693-227d7733a917?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 
     // add text content
@@ -13,6 +17,7 @@ const loadAbout = function() {
     // append chils
     let content = document.getElementById('content')
     content.appendChild(aboutContainer)
+    aboutContainer.appendChild(img)
     aboutContainer.appendChild(aboutUs)
     aboutContainer.appendChild(aboutContent)
 }
